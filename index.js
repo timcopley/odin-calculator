d1c782e88ffdc7d63a485fed24544e2725437c35
Tim Copley <249600077+timcopley@users.noreply.github.com>
2026-01-12 12:29:05 -0500

    Created basic operations

---
+++ index.js
@@ -0,0 +1,23 @@
+let numA
+let numB
+let operator
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate( a, b, operator) {
+    return operator(a, b);
+}
