ab3351dbd798252498bd1315ce498585d8d97e54
Tim Copley <249600077+timcopley@users.noreply.github.com>
2026-01-12 13:37:20 -0500

    Implementing ability to enter two numbers and a single operator

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ let operator;
 const numbers = document.querySelector('#numbers');
 numbers.addEventListener('click', numberClick);
 
+const operators = document.querySelector('#operators');
+operators.addEventListener('click', operatorClick);
+
 function add(a, b) {
     return a + b;
 }
@@ -25,12 +28,52 @@ function operate( a, b, operator) {
     return operator(a, b);
 }
 
+function compute() {
+
+}
+
+function resetCalculator() {
+    const display = document.querySelector('#display');
+    display.innerText = '';
+    numA = 0;
+    numB = 0;
+    operator = null;
+}
+
 function numberClick(event) {
     const display = document.querySelector('#display');
     const clickedNumber = event.target.innerText;
     console.log(clickedNumber);
 
-    display.innerText = display.innerText + clickedNumber;
+    display.innerText += clickedNumber;
 
-    numA = +(numA + clickedNumber);
+    if (!operator) {
+        numA = +(numA + clickedNumber);
+    } else {
+        numB = +(numB + clickedNumber);
+    }
+    
+}
+
+function operatorClick(event) {
+    const operators = {
+        '+': add,
+        '-': subtract,
+        'x': multiply,
+        '/': divide,
+        '=': compute,
+        'CLR': resetCalculator,
+    }
+    if (!operator) {
+        const display = document.querySelector('#display');
+        const clickedOperator = event.target.innerText;
+        console.log(clickedOperator);
+
+        operator = operators[clickedOperator];
+        display.innerText += clickedOperator;
+    } else {
+
+    }
+
+
 }
