a88721f114ae9faeb200d3b4d25d1cfdc07fdb90
Tim Copley <249600077+timcopley@users.noreply.github.com>
2026-01-12 13:04:00 -0500

    Added event handler for clicking numbers. Updates the display and the value of numA

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-let numA
-let numB
-let operator
+let numA = 0;
+let numB = 0;
+let operator;
+
+const numbers = document.querySelector('#numbers');
+numbers.addEventListener('click', numberClick);
 
 function add(a, b) {
     return a + b;
@@ -20,4 +23,14 @@ function divide(a, b) {
 
 function operate( a, b, operator) {
     return operator(a, b);
+}
+
+function numberClick(event) {
+    const display = document.querySelector('#display');
+    const clickedNumber = event.target.innerText;
+    console.log(clickedNumber);
+
+    display.innerText = display.innerText + clickedNumber;
+
+    numA = +(numA + clickedNumber);
 }
